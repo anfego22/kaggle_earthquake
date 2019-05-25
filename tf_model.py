a58ba856102987d65f61f6ab4b45b58d3ecbdb12
Andres Gonzalez <anfego22@gmail.com>
2019-05-24 22:17:09 -0500

    Extract the sample of data ids before the model train in order to reduce some training time.
    Generate the third_submission.csv file that scores 1.81 in LB and ~2.3 in CV.

--- tf_model.py
+++ tf_model.py
@@ -9,36 +9,26 @@ from sklearn.preprocessing import StandardScaler
 
 PATH = '/home/anfego/Documents/Kaggle/Earthquake/Data/Test/'
 BATCH_SIZE = 10
-WINDOW = 1400
+EPOCH = 32
 N = 150000
-FEATURES = 6
 MODEL_NAME = 'initial_model'
-new_model = True
+FEATURES = 1
 
 
-def get_sample_point():
+def get_sample_point(n):
     conection = pymongo.MongoClient()
     db = conection.earthquake
     cursor = db.train.aggregate([{
-        '$sample': {'size': BATCH_SIZE}}])
+        '$sample': {'size': n}}])
     ids = [el['_id'] for el in cursor]
     conection.close()
     return ids
 
 
-def gen_features(features):
-    features['mean'] = features['acoustic_data'].rolling(WINDOW).mean()
-    features['sd'] = features['acoustic_data'].rolling(WINDOW).std()
-    features['max'] = features['acoustic_data'].rolling(WINDOW).max()
-    features['min'] = features['acoustic_data'].rolling(WINDOW).min()
-    features['abs'] = features['acoustic_data'].abs()
-
-
 def preprocess_data(cursor, scaler):
     data = pd.DataFrame([el for el in cursor])
-    data['acoustic_data'] = scaler.fit_transform(data['acoustic_data'].\
+    data['acoustic_data'] = scaler.fit_transform(data['acoustic_data'].
                                                  values.reshape(-1, 1))
-    gen_features(data)
     features = data[data.columns[data.columns != 'time_to_failure']].values
     features = features[~np.isnan(features).any(axis=1)]
     labels = data['time_to_failure'].values[-1]
@@ -49,19 +39,20 @@ def get_data():
     while True:
         conection = pymongo.MongoClient()
         db = conection.earthquake
-        ids = get_sample_point()
         features_batch = []
+        global ids
         label_batch = []
         scaler = StandardScaler()
-        for _id in ids:
+        for _ in range(BATCH_SIZE):
+            _id = ids.pop()
             docs = db.train.find({'_id': {'$lte': _id}}, {'_id': 0}).\
                    sort([('_id', -1)]).limit(N)
             features, labels = preprocess_data(docs, scaler)
-            while features.shape[0] != (N - WINDOW + 1):
+            while features.shape[0] != N:
                 cursor = db.train.aggregate([{
-                    '$sample': {'size': BATCH_SIZE}}])
-                ids = [el['_id'] for el in cursor]
-                docs = db.train.find({'_id': {'$lte': _id}}, {'_id': 0}).\
+                    '$sample': {'size': 1}}])
+                new_id = [el['_id'] for el in cursor]
+                docs = db.train.find({'_id': {'$lte': new_id}}, {'_id': 0}).\
                     sort([('_id', -1)]).limit(N)
                 features, labels = preprocess_data(docs, scaler)
             features_batch.append(np.expand_dims(features, axis=0))
@@ -75,29 +66,26 @@ def get_data():
 # Looks it take a constant time no matter how many documents you pull
 ds = tf.data.Dataset.from_generator(get_data,
                                     output_types=(tf.float32, tf.float32),
-                                    output_shapes=((BATCH_SIZE, N - WINDOW + 1,
+                                    output_shapes=((BATCH_SIZE, N,
                                                     FEATURES),
                                                    (BATCH_SIZE, 1)))
 
 
 # Model
-if new_model:
-    model = k.Sequential([k.layers.InputLayer(input_shape=(N - WINDOW + 1,
-                                                           FEATURES)),
-                          k.layers.Conv1D(10, kernel_size= 100, strides=1,
-                                          activation='relu'),
-                          k.layers.Conv1D(10, kernel_size=100, strides=100,
-                                          activation='relu'),
-                          k.layers.Conv1D(100, kernel_size=100, strides=100),
-                          k.layers.Flatten(),
-                          k.layers.Dense(64, activation='relu'),
-                          k.layers.Dropout(0.1),
-                          k.layers.Dense(10, activation='relu'),
-                          k.layers.BatchNormalization(),
-                          k.layers.Dense(1, activation='linear')])
+model = k.Sequential([k.layers.InputLayer(input_shape=(N, 1)),
+                      k.layers.Conv1D(10, kernel_size= 100, strides=1,
+                                      activation='relu'),
+                      k.layers.Conv1D(10, kernel_size=100, strides=100,
+                                      activation='relu'),
+                      k.layers.Conv1D(100, kernel_size=100, strides=100),
+                      k.layers.Flatten(),
+                      k.layers.Dense(10, activation='relu'),
+                      k.layers.Dense(1, activation='linear')])
+model.compile(optimizer='adam', loss='mae')
 model.summary()
-model.compile(optimizer=k.optimizers.RMSprop(1e-3), loss='mae')
-model.fit(ds, epochs=1, steps_per_epoch=528)
+for i in range(10):
+    ids = get_sample_point(BATCH_SIZE*EPOCH)
+    model.fit(ds, epochs=1, steps_per_epoch=EPOCH)
 
 
 submission = []
@@ -107,7 +95,6 @@ for f in os.listdir(PATH):
     data = pd.read_csv(PATH + f, sep=',')
     data['acoustic_data'] = scaler.fit_transform(data['acoustic_data'].\
                                                  values.reshape(-1, 1))
-    gen_features(data)
     features = data[data.columns[data.columns != 'time_to_failure']].values
     features = features[~np.isnan(features).any(axis=1)]
     features = np.expand_dims(features, axis=0)
@@ -116,4 +103,4 @@ for f in os.listdir(PATH):
     submission.append(res)
 
 submission_pd = pd.DataFrame(submission)
-submission_pd.to_csv('Submission/second_submission.csv', index=False)
+submission_pd.to_csv('Submission/third_submission.csv', index=False)
